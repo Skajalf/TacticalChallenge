2c771e5003d6169a367fee90c21564a8f04dd4ec
Skajalf <skajalf@naver.com>
2024-08-27 11:39:58 +0900

    24.08.27

--- Tactical Challenge/Assets/Scripts/Character/Components/SkillComponent.cs
+++ Tactical Challenge/Assets/Scripts/Character/Components/SkillComponent.cs
@@ -6,13 +6,22 @@ using UnityEngine.InputSystem;
 public class SkillComponent : MonoBehaviour
 {
     private Animator animator;
+    [SerializeField] private GameObject weapon; // Bip001_Weapon을 참조할 변수
 
     public bool IsEXSkill { private set; get; }
     public bool IsNormalSkill { private set; get; }
+    public bool IsMeleeAttack { private set; get; }
 
     public void Awake()
     {
         animator = GetComponent<Animator>();
+
+        weapon = transform.FindChildByName("Bip001_Weapon")?.gameObject;
+
+        if (weapon == null)
+        {
+            Debug.LogError("Weapon not found! Make sure the weapon is named 'Bip001_Weapon'.");
+        }
 
         PlayerInput input = GetComponent<PlayerInput>();
 
@@ -20,12 +29,59 @@ public class SkillComponent : MonoBehaviour
 
         InputAction EXSkill = actionMap.FindAction("EXSkill");
         EXSkill.started += startEXSkill;
+
+        InputAction MeleeAttack = actionMap.FindAction("MeleeAttack");
+        MeleeAttack.started += startMeleeAttack;
     }
 
     private void startEXSkill(InputAction.CallbackContext context)
     {
         IsEXSkill = true;
+
+        if (weapon != null)
+        {
+            weapon.SetActive(false);
+        }
+
         animator.SetTrigger("EXSkill");
+        StartCoroutine(EndSkill());
+    }
+
+    private void startMeleeAttack(InputAction.CallbackContext context)
+    {
+        IsMeleeAttack = true;
+
+        if (weapon != null)
+        {
+            weapon.SetActive(false);
+        }
+
+        animator.SetTrigger("MeleeAttack");
+
+        StartCoroutine(EndMeleeAttack());
+    }
+
+    private IEnumerator EndMeleeAttack()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        if (weapon != null)
+        {
+            weapon.SetActive(true);
+        }
+
+        IsMeleeAttack = false;
+    }
+
+    private IEnumerator EndSkill()
+    {
+        yield return new WaitForSeconds(4.0f);
+
+        if (weapon != null)
+        {
+            weapon.SetActive(true);
+        }
+
         IsEXSkill = false;
     }
 }
