71355209569448c98ffdbf702a1bec7cdba38650
Skajalf <skajalf@naver.com>
2024-08-29 13:10:03 +0900

    24.08.29

--- Tactical Challenge/Assets/Scripts/Character/Components/MovingComponent.cs
+++ Tactical Challenge/Assets/Scripts/Character/Components/MovingComponent.cs
@@ -1,6 +1,6 @@
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
 
 public class MovingComponent : MonoBehaviour
 {
@@ -10,15 +10,33 @@ public class MovingComponent : MonoBehaviour
     [SerializeField] private float jumpForce = 1.2f;
     [SerializeField] private float gravity = -9.81f;
 
+    [Header("Camera Settings")]
+    [SerializeField] private bool bUseCamera = true;
+    [SerializeField] private string followTargetName = "Camera Root";
+    [SerializeField] private Vector2 mouseSensitivity = new Vector2(0.5f, 0.5f);
+    [SerializeField] private Vector2 limitPitchAngle = new Vector2(80, 350);
+    //[SerializeField] private float mouseRotationLerp = 0.25f;
+    [SerializeField] private float zoomSensitivity = 0.1f;
+    [SerializeField] private float zoomLerp = 25.0f;
+    [SerializeField] private Vector2 zoomRange = new Vector2(1, 3);
+
+    private Vector2 inputLook;
+    private float zoomDistance;
+    private Transform followTargetTransform;
+
+    private Quaternion rotation;
+    //public Quaternion Rotation { set => rotation = value; }
+
     [Header("Tech Settings")]
     [SerializeField] private float sensitivity = 100.0f;
-    [SerializeField] private float deadZone = 0.001f;
+    [SerializeField] private float deadZone = 0.01f;
     private Vector2 velocity;
 
     private Vector2 inputMove;
     private Vector2 currInputMove;
     private Vector3 verticalVelocity = Vector3.zero;
 
+    private Cinemachine3rdPersonFollow tpsFollowCamera;
     private CharacterController controller;
     private Animator animator;
 
@@ -45,6 +63,38 @@ public class MovingComponent : MonoBehaviour
 
         InputAction jump = actionMap.FindAction("Jump");
         jump.started += startJump;
+
+        InputAction lookAction = actionMap.FindAction("Look");
+        lookAction.performed += Input_Look_Performed;
+        lookAction.canceled += Input_Look_Canceled;
+
+        InputAction zoomAction = actionMap.FindAction("Zoom");
+        zoomAction.performed += Input_Zoom_Performed;
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        followTargetTransform = transform.FindChildByName(followTargetName);
+
+
+        CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
+        CinemachineVirtualCamera camera = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+        tpsFollowCamera = camera.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+
+        if (tpsFollowCamera != null)
+            zoomDistance = tpsFollowCamera.CameraDistance;
+    }
+
+    private void Update()
+    {
+        if (bUseCamera)
+        {
+            Update_RotateCamera();
+            Update_ZoomCamera();
+        }
     }
 
     public void FixedUpdate()
@@ -79,6 +129,62 @@ public class MovingComponent : MonoBehaviour
         bJump = true;
     }
 
+    private void Input_Look_Performed(InputAction.CallbackContext context)
+    {
+        inputLook = context.ReadValue<Vector2>();
+    }
+
+    private void Input_Look_Canceled(InputAction.CallbackContext context)
+    {
+        inputLook = Vector2.zero;
+    }
+
+    private void Input_Zoom_Performed(InputAction.CallbackContext context)
+    {
+        float value = -context.ReadValue<float>() * zoomSensitivity;
+
+        zoomDistance += value;
+        zoomDistance = Mathf.Clamp(zoomDistance, zoomRange.x, zoomRange.y);
+    }
+
+    private void Update_RotateCamera()
+    {
+        rotation *= Quaternion.AngleAxis(inputLook.x * mouseSensitivity.x, Vector3.up);
+        rotation *= Quaternion.AngleAxis(-inputLook.y * mouseSensitivity.y, Vector3.right);
+        followTargetTransform.rotation = rotation;
+
+        Vector3 angles = rotation.eulerAngles;
+        angles.z = 0.0f;
+
+        float xAngle = rotation.eulerAngles.x;
+
+        if (xAngle < 180.0f && xAngle > limitPitchAngle.x)
+            angles.x = limitPitchAngle.x;
+        else if (xAngle > 180.0f && xAngle < limitPitchAngle.y)
+            angles.x = limitPitchAngle.y;
+
+        rotation.eulerAngles = angles;
+
+        if (currInputMove.magnitude > deadZone)
+        {
+            transform.rotation = Quaternion.Euler(0, rotation.eulerAngles.y, 0);
+            followTargetTransform.localEulerAngles = new Vector3(angles.x, 0, 0);
+        }
+
+    }
+
+    private void Update_ZoomCamera()
+    {
+        if (MathHelpers.IsNearlyEqual(tpsFollowCamera.CameraDistance, zoomDistance, 0.01f))
+        {
+            tpsFollowCamera.CameraDistance = zoomDistance;
+
+            return;
+        }
+
+        tpsFollowCamera.CameraDistance = Mathf.SmoothStep(tpsFollowCamera.CameraDistance, zoomDistance, zoomLerp * Time.deltaTime);
+    }
+
     public void Movement()
     {
         if (!bCanMove) return;
@@ -90,8 +196,20 @@ public class MovingComponent : MonoBehaviour
 
         if (currInputMove.magnitude > deadZone)
         {
-            direction = (Vector3.right * currInputMove.x) + (Vector3.forward * currInputMove.y);
+            Vector3 forward = followTargetTransform.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = followTargetTransform.right;
+            right.y = 0;
+            right.Normalize();
+
+            direction = (right * currInputMove.x) + (forward * currInputMove.y);
             direction = direction.normalized * speed;
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * sensitivity);
+
             animator.SetBool("IsMove", true);
         }
         else
@@ -104,14 +222,7 @@ public class MovingComponent : MonoBehaviour
             verticalVelocity.y = 0f;
         }
 
-        if (direction.magnitude > 0.1f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * sensitivity);
-        }
-
         Vector3 move = direction * Time.deltaTime;
-        
         move.y = verticalVelocity.y * Time.deltaTime;
 
         controller.Move(move);
--- Tactical Challenge/Assets/Scripts/Helpers.cs
+++ Tactical Challenge/Assets/Scripts/Helpers.cs
@@ -15,3 +15,16 @@ public static class Extend_TransformHelpers
         return null;
     }
 }
+
+public static class MathHelpers
+{
+    public static bool IsNearlyEqual(float a, float b, float tolerance = 1e-6f)
+    {
+        return Mathf.Abs(a - b) <= tolerance;
+    }
+
+    public static bool IsNearlyZero(float a, float b, float tolerance = 1e-6f)
+    {
+        return Mathf.Abs(a) <= tolerance;
+    }
+}
