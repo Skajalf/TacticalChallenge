11c47abf483bee7b410b279b551237d5dee3420e
Skajalf <skajalf@naver.com>
2024-09-21 20:24:16 +0900

    24.09.21
    TacticalChallenge 리부트

--- Tactical Challenge/Assets/Scripts/Systems/IStoppable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-public interface IStoppable
-{
-    void Regist_MovableStopper();
-    void Remove_MovableStopper();
-    IEnumerator Start_FrameDelay(int frame);
-}
